9fa9d48da1cc8372b74aae34527fdbf866c70523
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-21 15:04:08 +0530

    bugs: minor image bug

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react' 
-import { View, ScrollView } from 'react-native'
-// import Banner1 from '@/components/home/Banner1'
+import { ScrollView } from 'react-native'
+import Banner1 from '@/components/home/Banner1'
 import Categories from '@/components/home/Categories'
 import FeaturedProducts from '@/components/home/FeaturedProducts'
 import TrendingNow from '@/components/home/TrendingNow'
@@ -9,7 +9,7 @@ import Banner2 from '@/components/home/Banner2'
 const Index = () => {
   return (
     <ScrollView>
-      {/* <Banner1 /> */}
+      <Banner1 />
       <Categories />
       <FeaturedProducts />
       <TrendingNow />
--- components/home/Banner1.tsx
+++ components/home/Banner1.tsx
@@ -1,39 +1,33 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Image } from "expo-image";
-import banner1 from "@/assets/images/home/banner1.png";
+import { Image } from 'expo-image';
+import { StyleSheet, View } from 'react-native';
 
 const blurhash =
-  "|ZGbbbx]WFRjWCxuxtxukC~pxuRkkCayWBaya|ofIUt7WBt7oeRkNGM{juIUoeoLWCazayaxWBRjRjoeofRjWBbHofs:f6oJWBa#ayj[ofofj[j[s.RjR%jsj[ofj[a#azoJWBWBa}j[j@ayj]j[ofj[j[kCaxayf6j[kC";
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
-const Banner1 = () => {
+export default function App() {
   return (
     <View style={styles.container}>
       <Image
-        // className="h-96 w-5"
         style={styles.image}
-        source={banner1}
-        placeholder={blurhash}
+        source="https://picsum.photos/seed/696/3000/2000"
+        placeholder={{ blurhash }}
         contentFit="cover"
         transition={1000}
       />
     </View>
   );
-};
-
-export default Banner1;
-
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    image: {
-      flex: 1,
-      width: '100%',
-      backgroundColor: '#0553',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },
+});
